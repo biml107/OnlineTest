ea75f08c0d92d17ac0f6a85f67883e69e5c6af61
Bimal <54972204+biml107@users.noreply.github.com>
2022-02-15 08:16:02 +0530

    Create OnlineTest

---
+++ src/OnlineTest.java
@@ -0,0 +1,208 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+class OnlineTest extends JFrame implements ActionListener
+{
+    JLabel l;
+    JRadioButton jb[]=new JRadioButton[5];
+    JButton b1,b2;
+    ButtonGroup bg;
+    int count=0,current=0,x=1,y=1,now=0;
+    int m[]=new int[10];
+    OnlineTest(String s)
+    {
+        super(s);
+        l=new JLabel();
+        add(l);
+        bg=new ButtonGroup();
+        for(int i=0;i<5;i++)
+        {
+            jb[i]=new JRadioButton();
+            add(jb[i]);
+            bg.add(jb[i]);
+        }
+        b1=new JButton("Next");
+        b2=new JButton("Bookmark");
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        add(b1);add(b2);
+        set();
+        l.setBounds(30,40,450,20);
+        jb[0].setBounds(50,80,800,20);
+        jb[1].setBounds(50,110,800,20);
+        jb[2].setBounds(50,140,800,20);
+        jb[3].setBounds(50,170,800,20);
+        b1.setBounds(100,240,100,30);
+        b2.setBounds(270,240,100,30);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setLocation(250,100);
+        setVisible(true);
+        setSize(800,400);
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==b1)
+        {
+            if(check())
+                count=count+1;
+            current++;
+            set();
+            if(current==9)
+            {
+                b1.setEnabled(false);
+                b2.setText("Result");
+            }
+        }
+        if(e.getActionCommand().equals("Bookmark"))
+        {
+            JButton bk=new JButton("Bookmark"+x);
+            bk.setBounds(480,20+30*x,100,30);
+            add(bk);
+            bk.addActionListener(this);
+            m[x]=current;
+            x++;
+            current++;
+            set();
+            if(current==9)
+                b2.setText("Result");
+            setVisible(false);
+            setVisible(true);
+        }
+        for(int i=0,y=1;i<x;i++,y++)
+        {
+            if(e.getActionCommand().equals("Bookmark"+y))
+            {
+                if(check())
+                    count=count+1;
+                now=current;
+                current=m[y];
+                set();
+                ((JButton)e.getSource()).setEnabled(false);
+                current=now;
+            }
+        }
+
+        if(e.getActionCommand().equals("Result"))
+        {
+            if(check())
+                count=count+1;
+            current++;
+            //System.out.println("correct ans="+count);
+            JOptionPane.showMessageDialog(this,"correct ans="+count);
+            System.exit(0);
+        }
+    }
+    void set()
+    {
+        jb[4].setSelected(true);
+        if(current==0)
+        {
+            l.setText("Que1:Which of the following saLts does not contain water of crystatUsatlon?");
+            jb[0].setText("Blue vitriol");jb[1].setText("Baking soda");jb[2].setText("Washing soda");jb[3].setText("Gypsum");
+        }
+        if(current==1)
+        {
+            l.setText("Que2: Baking soda is a mixture of:");
+            jb[0].setText("Sodium carbonate and acetic acid");
+            jb[1].setText("Sodium carbonate and tartaric acid");
+            jb[2].setText("Sodium hydrogen carbonate and tartaric acid");
+            jb[3].setText(" Sodium hydrogen carbonate and acetic acid");
+        }
+        if(current==2)
+        {
+            l.setText("Que3: Which of the following gives the correct increasing order of acid strength?");
+            jb[0].setText("Water < acetic acid < hydrochloric acid");
+            jb[1].setText(" Water < hydrochloric acid < acetic acid");
+            jb[2].setText(" Acetic acid < water < hydrochloric acid");
+            jb[3].setText("Hydrochloric acid < water < acetic acid");
+        }
+        if(current==3)
+        {
+            l.setText("Que4: Which of the following is not a mineral acid?");
+            jb[0].setText(" Hydrochloric acid");
+            jb[1].setText("Citric acid");
+            jb[2].setText("Sulphuric acid");
+            jb[3].setText("Nitric acid");
+        }
+        if(current==4)
+        {
+            l.setText("Que5: Which among the following is not a base?");
+            jb[0].setText("NaOH");
+            jb[1].setText("KOH");
+            jb[2].setText("NH4OH");
+            jb[3].setText("C2H5OH");
+        }
+        if(current==5)
+        {
+            l.setText("Que6:Which of the following statements is true for acids?");
+            jb[0].setText("Bitter and change red litmus to blue.");
+            jb[1].setText(" Sour and change red litmus to blue.");
+            jb[2].setText("Sour and change blue litmus to red.");
+            jb[3].setText("Bitter and change blue litmus to red.");
+        }
+        if(current==6)
+        {
+            l.setText("Que7:Which of the following solutions in water does not conduct electricity? ");
+            jb[0].setText("Hydrochloric acid");
+            jb[1].setText("Sodium chloride");
+            jb[2].setText("Glucose");
+            jb[3].setText(" Sulphuric acid [Diksha]");
+        }
+        if(current==7)
+        {
+            l.setText("Que8:Which one of the following metals does not react with cold as well as hot water?");
+            jb[0].setText("Na");
+            jb[1].setText("Ca");
+            jb[2].setText("Mg");
+            jb[3].setText("Fe");
+        }
+        if(current==8)
+        {
+            l.setText("Que9: Which of the foLLowing are not ionic compounds?");
+            jb[0].setText("KCL");
+            jb[1].setText("HCL");
+            jb[2].setText("CCL4");
+            jb[3].setText("NaCl");
+        }
+        if(current==9)
+        {
+            l.setText("Que10: Galvanisation is a method of protecting iron from rusting by coating it with a thin layer of:");
+            jb[0].setText(" Gallium");
+            jb[1].setText(" Aluminium");
+            jb[2].setText("Zinc");
+            jb[3].setText("Silver");
+        }
+        l.setBounds(30,40,450,20);
+        for(int i=0,j=0;i<=90;i+=30,j++)
+            jb[j].setBounds(50,80+i,400,20);
+    }
+    boolean check()
+    {
+        if(current==0)
+            return(jb[1].isSelected());
+        if(current==1)
+            return(jb[2].isSelected());
+        if(current==2)
+            return(jb[3].isSelected());
+        if(current==3)
+            return(jb[0].isSelected());
+        if(current==4)
+            return(jb[2].isSelected());
+        if(current==5)
+            return(jb[2].isSelected());
+        if(current==6)
+            return(jb[1].isSelected());
+        if(current==7)
+            return(jb[3].isSelected());
+        if(current==8)
+            return(jb[1].isSelected());
+        if(current==9)
+            return(jb[2].isSelected());
+        return false;
+    }
+    public static void main(String s[])
+    {
+        new OnlineTest("Online Test Of Science");
+    }
+}
